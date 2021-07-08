68e34b5c482ad02220de0fc21f3e8f0cc328cc7a
Brianna Gordon <brigordon574@gmail.com>
2021-07-07 19:50:02 -0700

    commit tree 108371488d715cd2dc3b3731e7194548b598a343

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, category TEXT, funding_goal INTEGER,start_date DATE, end_date DATE);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER,user_id INTEGER FOREIGN KEY, project_id INTEGER FOREIGN KEY);
